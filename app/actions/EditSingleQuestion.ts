8e54636d743734996027f2088fe0a574ad90cda8
Rahul Gupta <rg5353070@gmail.com>
2024-09-19 10:56:22 +0530

    changed edit single question

--- app/actions/EditSingleQuestion.ts
+++ app/actions/EditSingleQuestion.ts
@@ -1,53 +1,55 @@
-"use server";
+// "use server";
 
-import db from "@/lib/db";
-import { Question } from "@/lib/types";
-import { revalidatePath } from "next/cache";
+// import db from "@/lib/db";
+// import { Question } from "@/lib/types";
+// import { revalidatePath } from "next/cache";
 
-const editSingleQuestion = async (values: Question, examId: string) => {
-  let option1 = values.option1;
-  let option2 = values.option2;
-  let option3 = values.option3;
-  let option4 = values.option4;
-  let options = [
-    {
-      option1,
-    },
-    {
-      option2,
-    },
-    {
-      option3,
-    },
-    {
-      option4,
-    },
-  ];
-  console.log("values", values);
-  console.log("options", options);
+export default async function editSingleQuestion() {}
 
-  try {
-    await db.question.update({
-      where: {
-        id: values.id,
-      },
-      data: {
-        question: values.question,
-        answer: values.answer,
-        options: JSON.stringify(options),
-      },
-    });
+// const editSingleQuestion = async (values: Question, examId: string) => {
+//   let option1 = values.option1;
+//   let option2 = values.option2;
+//   let option3 = values.option3;
+//   let option4 = values.option4;
+//   let options = [
+//     {
+//       option1,
+//     },
+//     {
+//       option2,
+//     },
+//     {
+//       option3,
+//     },
+//     {
+//       option4,
+//     },
+//   ];
+//   console.log("values", values);
+//   console.log("options", options);
 
-    revalidatePath(`/exam/${examId}/`);
+//   try {
+//     await db.question.update({
+//       where: {
+//         id: values.id,
+//       },
+//       data: {
+//         question: values.question,
+//         answer: values.answer,
+//         options: JSON.stringify(options),
+//       },
+//     });
 
-    return {
-      success: "successfully edited question",
-    };
-  } catch (error) {
-    console.log("error while editing question in server action", error);
-    return {
-      error: "Could not edit question",
-    };
-  }
-};
-export default editSingleQuestion;
+//     revalidatePath(`/exam/${examId}/`);
+
+//     return {
+//       success: "successfully edited question",
+//     };
+//   } catch (error) {
+//     console.log("error while editing question in server action", error);
+//     return {
+//       error: "Could not edit question",
+//     };
+//   }
+// };
+// export default editSingleQuestion;
